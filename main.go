58e1f4c34782d7d6eca1cf5bc3040205f1d97faf
Sapardi <sapardee@gmail.com>
2019-09-10 15:07:31 +0700

    update

---
+++ main.go
@@ -0,0 +1,18 @@
+package gomodmultiplies
+
+func Multiply(items ...int64) int64 {
+	res := int64(1)
+	for _, item := range items {
+		res *= item
+	}
+
+	return res
+}
+
+func MultiplyInt(items ...int) int64 {
+	res := 1
+	for _, item := range items {
+	 res *= item
+	}
+	return int64(res)
+   }
